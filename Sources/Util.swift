84143994e2598f49817ad341df19dc9d6b6b168a
Rintaro Ishizaki <fs.output@gmail.com>
2016-11-01 17:56:18 +0900

    Fix file header

--- Sources/Util.swift
+++ Sources/Util.swift
@@ -1,15 +1,11 @@
-//
-//  Util.swift
-//  JSON
-//
-//  Created by rintaro on 10/31/16.
-//
-//
-
+/// In optimized configuration, statically convert an ASCII character literal to
+/// UTF16 CodeUnit (UInt16)
 func ascii16(_ x: UnicodeScalar) -> UTF16.CodeUnit {
     return UTF16.CodeUnit(truncatingBitPattern: x.value)
 }
 
+/// In optimized configuration, statically convert an ASCII character literal to
+/// UTF8 CodeUnit (UInt8)
 func ascii8(_ x: UnicodeScalar) -> UTF8.CodeUnit {
     return UTF8.CodeUnit(truncatingBitPattern: x.value)
 }
