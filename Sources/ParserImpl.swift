a81625b623c6881617ff9176e62039424ad38b78
Rintaro Ishizaki <fs.output@gmail.com>
2016-11-01 16:30:46 +0900

    Parser: fixed a bug introduced in 9e24e20d

--- Sources/ParserImpl.swift
+++ Sources/ParserImpl.swift
@@ -358,8 +358,8 @@ extension Lexer {
                 let n: UTF16.CodeUnit
                 switch c {
                 case digit: n = UTF16.CodeUnit(c) &- digit.lowerBound
-                case lower: n = UTF16.CodeUnit(c) &- (lower.lowerBound + 10)
-                case upper: n = UTF16.CodeUnit(c) &- (upper.lowerBound + 10)
+                case lower: n = UTF16.CodeUnit(c) &- lower.lowerBound &+ 10
+                case upper: n = UTF16.CodeUnit(c) &- upper.lowerBound &+ 10
                 default:
                     hasError = true;
                     return nil

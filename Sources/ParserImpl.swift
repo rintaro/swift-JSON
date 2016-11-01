rintaro/swift-JSON

commit 9e24e20d2553c2c87bc9588131ce1bbbfc38d436

optimization

Sources/ParserImpl.swift
@@ -348,9 +348,9 @@ extension Lexer {
                 let c = UTF16.CodeUnit(ptr.pointee)
                 let n: UTF16.CodeUnit
                 switch c {
-                case digit: n = UTF16.CodeUnit(c) - digit.lowerBound
-                case lower: n = UTF16.CodeUnit(c) - lower.lowerBound + 10
-                case upper: n = UTF16.CodeUnit(c) - upper.lowerBound + 10
+                case digit: n = UTF16.CodeUnit(c) &- digit.lowerBound
+                case lower: n = UTF16.CodeUnit(c) &- (lower.lowerBound + 10)
+                case upper: n = UTF16.CodeUnit(c) &- (upper.lowerBound + 10)
                 default:
                     hasError = true;
                     return nil

Sources/PrinterImpl.swift
@@ -66,7 +66,7 @@ struct PrinterImpl<NullType> {
             flush()
         }
         buf[bufIdx] = chr
-        bufIdx += 1
+        bufIdx = bufIdx &+ 1
     }
 
     mutating func put<S : Sequence>(_ seq: S)

commit a81625b623c6881617ff9176e62039424ad38b78

Parser: fixed a bug introduced in 9e24e20d

Sources/ParserImpl.swift
@@ -358,8 +358,8 @@ extension Lexer {
                 let n: UTF16.CodeUnit
                 switch c {
                 case digit: n = UTF16.CodeUnit(c) &- digit.lowerBound
-                case lower: n = UTF16.CodeUnit(c) &- (lower.lowerBound + 10)
-                case upper: n = UTF16.CodeUnit(c) &- (upper.lowerBound + 10)
+                case lower: n = UTF16.CodeUnit(c) &- lower.lowerBound &+ 10
+                case upper: n = UTF16.CodeUnit(c) &- upper.lowerBound &+ 10
                 default:
                     hasError = true;
                     return nil